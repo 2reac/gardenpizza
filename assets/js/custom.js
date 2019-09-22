cfb4e4623978db5fc6fcfb8f7853da5482f4f9a3
Radu Tureac <radu.2reac@yahoo.com>
2019-09-22 19:42:55 +0300

    fade/scroll + css variables

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -201,17 +201,17 @@ jQuery(function($){
   //Check to see if the window is top if not then display button
 
     jQuery(window).scroll(function(){
-      if (jQuery(this).scrollTop() > 300) {
-        jQuery('.scrollToTop').fadeIn();
+      if (jQuery(this).scrollTop() > 500) {
+        jQuery('.scrollToTop').fadeIn(500);
       } else {
-        jQuery('.scrollToTop').fadeOut();
+        jQuery('.scrollToTop').fadeOut(500);
       }
     });
      
     //Click event to scroll to top
 
     jQuery('.scrollToTop').click(function(){
-      jQuery('html, body').animate({scrollTop : 0},800);
+      jQuery('html, body').animate({scrollTop : 0},999);
       return false;
     });
   

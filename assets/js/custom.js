c424d264320bebe7e786683cdcaee8114b81e017
Radu Tureac <radu.2reac@yahoo.com>
2019-10-09 17:15:36 +0300

    about image transition ok

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -22,226 +22,237 @@
   
 **/
 
-jQuery(function($){
+jQuery(function ($) {
 
-  
+
   /* ----------------------------------------------------------- */
   /*  1. TOP SLIDER (SLICK SLIDER)
-  /* ----------------------------------------------------------- */    
+  /* ----------------------------------------------------------- */
 
-    jQuery('.gp-top-slider').slick({
-      dots: true,
-      infinite: true,
-      arrows: false,
-      speed: 500,     
-      autoplay: true,
-      fade: true,
-      cssEase: 'linear'
-    });
+  jQuery('.gp-top-slider').slick({
+    dots: true,
+    infinite: true,
+    arrows: false,
+    speed: 500,
+    autoplay: true,
+    fade: true,
+    cssEase: 'linear'
+  });
 
   /* ----------------------------------------------------------- */
   /*  2. PORTFOLIO POPUP VIEW ( IMAGE LIGHTBOX )
-  /* ----------------------------------------------------------- */ 
+  /* ----------------------------------------------------------- */
 
   $('.gp-imglink').magnificPopup({
     type: 'image',
     mainClass: 'mfp-fade',
-    gallery:{
-      enabled:true
+    gallery: {
+      enabled: true
     }
   });
 
   /* ----------------------------------------------------------- */
   /*  3. DATEPICKER
-  /* ----------------------------------------------------------- */      
+  /* ----------------------------------------------------------- */
 
-    jQuery('#datepicker').datepicker();
+  jQuery('#datepicker').datepicker();
 
   /* ----------------------------------------------------------- */
   /* 4. SHEF SLIDER (SLICK SLIDER)
-  /* ----------------------------------------------------------- */    
+  /* ----------------------------------------------------------- */
 
-    jQuery('.gp-chef-nav').slick({
-      dots: true,
-      arrows: false,
-      infinite: true,
-      speed: 300,
-      slidesToShow: 4,
-      slidesToScroll: 2,
-      autoplay: true,
-      autoplaySpeed: 2500,
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            infinite: true,
-            dots: true
-          }
-        },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
+  jQuery('.gp-chef-nav').slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 2,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
         }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
-      ]
-    });
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
 
   /* ----------------------------------------------------------- */
   /*  5. TESTIMONIAL SLIDER (SLICK SLIDER)
-  /* ----------------------------------------------------------- */    
+  /* ----------------------------------------------------------- */
 
-    jQuery('.gp-testimonial-slider').slick({
-      dots: true,      
-      infinite: true,
-      arrows: false,
-      autoplay: true,
-      speed: 500,      
-      cssEase: 'linear'
-    });       
+  jQuery('.gp-testimonial-slider').slick({
+    dots: true,
+    infinite: true,
+    arrows: false,
+    autoplay: true,
+    speed: 500,
+    cssEase: 'linear'
+  });
 
   /* ----------------------------------------------------------- */
   /*  6. GOOGLE MAP
-  /* ----------------------------------------------------------- */ 
-        
-    $('#gp-map').click(function () {
-        $('#gp-map iframe').css("pointer-events", "auto");
-    });
-    
-    $("#gp-map").mouseleave(function() {
-      $('#gp-map iframe').css("pointer-events", "none"); 
-    });
-    
+  /* ----------------------------------------------------------- */
 
-	
-	
+  $('#gp-map').click(function () {
+    $('#gp-map iframe').css("pointer-events", "auto");
+  });
+
+  $("#gp-map").mouseleave(function () {
+    $('#gp-map iframe').css("pointer-events", "none");
+  });
+
+
+
+
   /* ----------------------------------------------------------- */
   /*  7. MENU SMOOTH SCROLLING
-  /* ----------------------------------------------------------- */ 
-
-  
-    //MENU SCROLLING WITH ACTIVE ITEM SELECTED
-
-      // Cache selectors
-      var lastId,
-      topMenu = $(".gp-main-nav"),
-      topMenuHeight = topMenu.outerHeight()+13,
-      // All list items
-      menuItems = topMenu.find('a[href^=\\#]'),
-      // Anchors corresponding to menu items
-      scrollItems = menuItems.map(function(){
-        var item = $($(this).attr("href"));
-        if (item.length) { return item; }
-      });
-
-      // Bind click handler to menu items
-      // so we can get a fancy scroll animation
-      menuItems.click(function(e){
-        var href = $(this).attr("href"),
-            offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+32;
-        jQuery('html, body').stop().animate({ 
-            scrollTop: offsetTop
-        }, 1500);           
-         jQuery('.navbar-collapse').removeClass('in');  
-        e.preventDefault();
-      });
-
-      // Bind to scroll
-      jQuery(window).scroll(function(){
-         // Get container scroll position
-         var fromTop = $(this).scrollTop()+topMenuHeight;
-         
-         // Get id of current scroll item
-         var cur = scrollItems.map(function(){
-           if ($(this).offset().top < fromTop)
-             return this;
-         });
-         // Get the id of the current element
-         cur = cur[cur.length-1];
-         var id = cur && cur.length ? cur[0].id : "";
-         
-         if (lastId !== id) {
-             lastId = id;
-             // Set/remove active class
-             menuItems
-               .parent().removeClass("active")
-               .end().filter("[href=\\#"+id+"]").parent().addClass("active");
-         }           
-      })
-  
   /* ----------------------------------------------------------- */
-  /*  8. HOVER DROPDOWN MENU
-  /* ----------------------------------------------------------- */ 
-  
-  // for hover dropdown menu
-    jQuery('ul.nav li.dropdown').hover(function() {
-      jQuery(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(200);
-    }, function() {
-      jQuery(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(200);
+
+
+  //MENU SCROLLING WITH ACTIVE ITEM SELECTED
+
+  // Cache selectors
+  var lastId,
+    topMenu = $(".gp-main-nav"),
+    topMenuHeight = topMenu.outerHeight() + 13,
+    // All list items
+    menuItems = topMenu.find('a[href^=\\#]'),
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function () {
+      var item = $($(this).attr("href"));
+      if (item.length) { return item; }
     });
 
-    
+  // Bind click handler to menu items
+  // so we can get a fancy scroll animation
+  menuItems.click(function (e) {
+    var href = $(this).attr("href"),
+      offsetTop = href === "#" ? 0 : $(href).offset().top - topMenuHeight + 32;
+    jQuery('html, body').stop().animate({
+      scrollTop: offsetTop
+    }, 1500);
+    jQuery('.navbar-collapse').removeClass('in');
+    e.preventDefault();
+  });
+
+  // Bind to scroll
+  jQuery(window).scroll(function () {
+    // Get container scroll position
+    var fromTop = $(this).scrollTop() + topMenuHeight;
+
+    // Get id of current scroll item
+    var cur = scrollItems.map(function () {
+      if ($(this).offset().top < fromTop)
+        return this;
+    });
+    // Get the id of the current element
+    cur = cur[cur.length - 1];
+    var id = cur && cur.length ? cur[0].id : "";
+
+    if (lastId !== id) {
+      lastId = id;
+      // Set/remove active class
+      menuItems
+        .parent().removeClass("active")
+        .end().filter("[href=\\#" + id + "]").parent().addClass("active");
+    }
+  })
+
+  /* ----------------------------------------------------------- */
+  /*  8. HOVER DROPDOWN MENU
+  /* ----------------------------------------------------------- */
+
+  // for hover dropdown menu
+  jQuery('ul.nav li.dropdown').hover(function () {
+    jQuery(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(200);
+  }, function () {
+    jQuery(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(200);
+  });
+
+
   /* ----------------------------------------------------------- */
   /*  9. SCROLL TOP BUTTON
   /* ----------------------------------------------------------- */
 
   //Check to see if the window is top if not then display button
 
-    jQuery(window).scroll(function(){
-      if (jQuery(this).scrollTop() > 500) {
-        jQuery('.scrollToTop').fadeIn(500);
-      } else {
-        jQuery('.scrollToTop').fadeOut(500);
-      }
-    });
-     
-    //Click event to scroll to top
+  jQuery(window).scroll(function () {
+    if (jQuery(this).scrollTop() > 500) {
+      jQuery('.scrollToTop').fadeIn(500);
+    } else {
+      jQuery('.scrollToTop').fadeOut(500);
+    }
+  });
 
-    jQuery('.scrollToTop').click(function(){
-      jQuery('html, body').animate({scrollTop : 0},999);
-      return false;
-    });
-  
+  //Click event to scroll to top
+
+  jQuery('.scrollToTop').click(function () {
+    jQuery('html, body').animate({ scrollTop: 0 }, 999);
+    return false;
+  });
+
   /* ----------------------------------------------------------- */
   /*  10. BUTTON SMOOTH SCROLL ( VIEW MY WORK )
   /* ----------------------------------------------------------- */
 
-    $('.gp-reservation-btn').on('click',function (e) {
-          e.preventDefault();
-          var target = this.hash,
-          $target = $(target);
-          $('html, body').stop().animate({
-              'scrollTop': $target.offset().top
-          }, 1000, 'swing', function () {
-              window.location.hash = target;
-      });
+  $('.gp-reservation-btn').on('click', function (e) {
+    e.preventDefault();
+    var target = this.hash,
+      $target = $(target);
+    $('html, body').stop().animate({
+      'scrollTop': $target.offset().top
+    }, 1000, 'swing', function () {
+      window.location.hash = target;
+    });
   });
 
   /* ----------------------------------------------------------- */
   /*  11. MOBILE DEVICE PHONE NUMBER SWITCH
   /* ----------------------------------------------------------- */
-  
-  jQuery('#mobile-call').click(function(){
+
+  jQuery('#mobile-call').click(function () {
     $(".gp-switch").hide();
     $(".gp-switch-tel").show();
   });
-  jQuery('#mobile-menu').click(function(){
+  jQuery('#mobile-menu').click(function () {
     $(".gp-switch-tel").hide();
     $(".gp-switch").show();
   });
-  
-});
 
+
+  if ("ontouchstart" in document.documentElement) {
+    $(".gp-about-us-left img").click(function () {
+      $(".gp-about-us-left img.top").toggleClass("transparent");
+    });
+  }
+  else {
+    $(".gp-about-us-left img").hover(function () {
+      $(".gp-about-us-left img.top").toggleClass("transparent");
+    });
+  }
+
+});
